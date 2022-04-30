a000074d76f3cc1ea082473eb88ade88bc0e68c8
Mithun Koppal <mkoppal@vmware.com>
2022-04-30 23:01:15 +0530

    Changed the display text

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Pranav Koppal</p>
+        <p>Pranav Koppal Brigade School Bangalore</p>
         <a
           className="App-link"
           href="https://reactjs.org"
